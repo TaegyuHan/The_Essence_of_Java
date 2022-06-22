19031977457910c746e8a55c6535510465a11dee
한태규 <gksxorb147@gmail.com>
2022-06-22 19:44:16 +0900

    ch4

---
+++ src/ch4/FlowEX29.java
@@ -0,0 +1,19 @@
+package ch4;
+
+public class Flow29 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.printf("i=%d, ", i);
+
+            int tmp = i;
+
+            do {
+                if (tmp%10%3==0 && tmp%10!=0) {
+                    System.out.println("짝");
+                }
+            } while((tmp/=10)!=0);
+            System.out.println();
+        }
+    }
+}
---
+++ src/ch4/FlowEx16.java
@@ -0,0 +1,13 @@
+package ch4;
+
+public class FlowEx15 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j <= 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
